6b9482e7f682fb2f41f486a42303d4e47041767f
Marcus <146900979+marcuswu5@users.noreply.github.com>
2026-02-24 09:51:25 -0500

    Change returned values for name and Andrew ID queries

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,11 +8,11 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "marcusw";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
-	return ("andrewc");
+	return ("marcusw");
   }
 
   return "";
