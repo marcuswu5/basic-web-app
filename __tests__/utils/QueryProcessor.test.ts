65d10750faf6b4431b6297c45d35a559662566f7
Marcus <146900979+marcuswu5@users.noreply.github.com>
2026-02-24 10:20:41 -0500

    Remove specific tests from QueryProcessor
    Removed tests for name and Andrew ID responses from QueryProcessor.

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,20 +17,4 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
-
-    test('should return name', () => {
-        const query = "What is your name?";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "Rohan"
-          ));
-    })
-
-	test('should return andrewid', () => {
-        const query = "What is your Andrew ID?";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "andrewc"
-          ));
-    })
 });
